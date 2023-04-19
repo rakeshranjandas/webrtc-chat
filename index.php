05ce4595943ce7e3c0e77b725a9f1ea1bd0a4ac1
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-19 11:54:18 +0530

    Basic UI, Favicon

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>WebRTC Chat</title>
+	<link rel="icon" type="image/x-icon" href="/images/favicon.png">
+	<link href="styles/default.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+	<div id="user-id-div">
+		<p>
+			<label>Select User:
+				<select id="user-id-select">
+				</select>
+			</label>
+		</p>
+		<p>
+			<button onclick="chooseUser()">Enter</button>
+		</p>
+	</div>
+
+	<div id="send-receive-div">
+		<div>
+			<p id="send-receive-user-p">Current User: <span id="send-receive-user-span"></span></p>
+		</div>
+		<div id="send-receive-container">
+			<div id="send-div">
+				<h3>Send</h3>
+				<div id="send-div-send-area">
+					<input type="text" id="send-div-send-input" />
+					<p id="send-div-send-buttons-p">
+					</p>
+				</div>
+				<div id="send-div-sent-area">
+				</div>
+			</div>
+			<div id="receive-div">
+				<h3>Receive</h3>
+				<div id="receive-div-received">
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+	<script src="scripts/main.js"></script>
+
+</body>
+
+</html>
---
+++ scripts/main.js
@@ -0,0 +1,70 @@
+const numberOfUsers = 4
+var currentUser = 0
+
+$(document).ready(function () {
+  populateUserSelect()
+})
+
+function populateUserSelect() {
+  let optionStr = ""
+  for (let i = 1; i <= numberOfUsers; i++)
+    optionStr += `<option value="${i}">${i}</option>`
+
+  $("#user-id-select").html(optionStr)
+}
+
+function chooseUser() {
+  const userId = $("#user-id-select").val()
+  currentUser = userId
+  $("#send-receive-user-span").text(userId)
+  $("#send-receive-div").show()
+  $("#user-id-div").hide()
+
+  popuplateSendButtons()
+}
+
+function popuplateSendButtons() {
+  let buttonStr = ""
+  for (let i = 1; i <= numberOfUsers; i++) {
+    if (i == currentUser) continue
+
+    buttonStr += `<button onclick="sendToUser(${i})">Send to ${i}</button>`
+  }
+
+  $("#send-div-send-buttons-p").html(buttonStr)
+}
+
+function scrollToBottom(divToScroll) {
+  divToScroll.scrollTop(divToScroll.prop("scrollHeight"))
+}
+
+function sendToUser(toUser) {
+  const sendInput = $("#send-div-send-input")
+  const message = sendInput.val()
+
+  if (message === "") {
+    alert("Empty message")
+    return false
+  }
+  sendInput.val("")
+
+  // Send Action
+
+  appendToSent(toUser, message)
+  scrollToBottom($("#send-div-sent-area"))
+}
+
+function appendToSent(toUser, message) {
+  const str = `<p>To ${toUser}: ${message}</p>`
+  $("#send-div-sent-area").append(str)
+}
+
+function receivedFromUser(fromUser, message) {
+  appendToReceived(fromUser, message)
+  scrollToBottom($("#receive-div-received"))
+}
+
+function appendToReceived(fromUser, message) {
+  const str = `<p>From ${fromUser}: ${message}</p>`
+  $("#receive-div-received").append(str)
+}
