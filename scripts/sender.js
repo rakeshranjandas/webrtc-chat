71e6a3930e297c79fbb2382a633dffd8e2b40c9d
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2023-04-21 22:33:10 +0530

    Sending pending messages

--- scripts/sender.js
+++ scripts/sender.js
@@ -4,6 +4,7 @@ const sender = {
   sendMessage: function (to, message) {
     const channel = ChannelCache.get(to)
     if (channel !== undefined) {
+      console.log("sending message ", to, message)
       channel.send(message)
       return
     }
@@ -16,5 +17,24 @@ const sender = {
 
     this._pendingQueueMap.set(to, [message])
     sendSendRequest(to)
+  },
+
+  sendPendingMessagesInChannel: function (to) {
+    const pendingQueue = this._pendingQueueMap.get(to)
+
+    if (pendingQueue === undefined) {
+      console.error("Could not find to in _pendingQueueMap", to)
+      return
+    }
+
+    const channel = ChannelCache.get(to)
+
+    while (pendingQueue.length) {
+      let message = pendingQueue.shift()
+      console.log("sending message after channel first open", to, message)
+      channel.send(message)
+    }
+
+    this._pendingQueueMap.delete(to)
   },
 }
